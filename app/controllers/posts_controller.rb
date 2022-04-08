ef60ac59594f4c2ba7ea2e0632ef7c8701b1a76d
LoopFruits <Kensworldc@gmail.com>
2022-04-08 11:28:12 -0400

    completed assignment, with notes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  # added our rescuse from  here and in the private 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def show
     post = Post.find(params[:id])
@@ -6,10 +10,11 @@ class PostsController < ApplicationController
     render json: post
   end
 
+  #PATCH /posts/:id
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
+    # all we added was the ! 
+    post.update!(post_params)
 
     render json: post
   end
@@ -20,4 +25,11 @@ class PostsController < ApplicationController
     params.permit(:category, :content, :title)
   end
 
+  def render_not_found_response
+    render json: { error: "Author not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 end
